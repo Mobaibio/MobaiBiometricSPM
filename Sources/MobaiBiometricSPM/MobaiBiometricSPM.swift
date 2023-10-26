abf2fac274125dc708a3c696b58ec7212e0c39be
Dion Dula <dion.dula.ext@hacon.de>
2023-10-26 17:31:17 +0200

    init

--- Sources/MobaiBiometricSPM/MobaiBiometricSPM.swift
+++ Sources/MobaiBiometricSPM/MobaiBiometricSPM.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import MobaiBiometric
+
+extension MBCaptureSessionResult {
+    public func serializedData() throws -> Data {
+        var requestBody = Mobai_BiometricRequest()
+        requestBody.recognitionRequest.probeImage = faceImage
+        requestBody.padRequest.padData = padData
+        
+        return try requestBody.serializedData()
+    }
+}
