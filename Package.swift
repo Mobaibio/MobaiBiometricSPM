16c2568b1af8b603439052ac5cb2ad3abd9f0532
Dion Dula <dion.dula.ext@hacon.de>
2023-10-26 17:31:17 +0200

    init

--- Package.swift
+++ Package.swift
@@ -6,18 +6,23 @@ import PackageDescription
 let package = Package(
     name: "MobaiBiometricSPM",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MobaiBiometricSPM",
-            targets: ["MobaiBiometricSPM"]),
+            targets: ["MobaiBiometricSPM", "MobaiBiometric"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MobaiBiometricSPM"),
-        .testTarget(
-            name: "MobaiBiometricSPMTests",
-            dependencies: ["MobaiBiometricSPM"]),
+            name: "MobaiBiometricSPM",
+            dependencies: [
+                .target(name: "MobaiBiometric"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+            ]),
+        .binaryTarget(
+            name: "MobaiBiometric",
+            path: "./Sources/MobaiBiometric.xcframework"
+        ),
     ]
 )
