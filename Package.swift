37265070845526c1238bc90ac65f310419500d31
Dion Dula <dd@mobai.bio>
2024-11-22 15:18:24 +0100

    Adding new version of xcframework

--- Package.swift
+++ Package.swift
@@ -8,21 +8,28 @@ let package = Package(
     products: [
         .library(
             name: "MobaiBiometric",
-            targets: ["MobaiBiometric", "MobaiBiometricCore"]),
+            targets: ["MobaiBiometricCore"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0"),
+        .package(
+            url: "https://github.com/apple/swift-protobuf.git",
+            "1.19.0" ..< "2.0.0"
+        ),
     ],
     targets: [
+        .binaryTarget(
+            name: "MobaiBiometric",
+            url: "https://downloads.mobai.dev/releases/ios/2.1.2-beta.1/MobaiBiometric.xcframework.zip",
+            checksum: "09d7cb61ccbad59d237a9a0b722c5c13035da3bf1432b6acdf0ad5b8f1d01a86"
+        ),
         .target(
             name: "MobaiBiometricCore",
             dependencies: [
-                .target(name: "MobaiBiometric"),
+                "MobaiBiometric",
                 .product(name: "SwiftProtobuf", package: "swift-protobuf")
-            ]),
-        .binaryTarget(
-            name: "MobaiBiometric",
-            path: "./Sources/MobaiBiometric.xcframework"
+            ],
+            path: "Sources/MobaiBiometricCore" // or specify your custom path
         ),
     ]
 )
