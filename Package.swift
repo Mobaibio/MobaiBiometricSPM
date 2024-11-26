492a0d3e837fd9a786a6d76571c42d9d99048982
Martin <martin@mobai.bio>
2024-11-26 16:04:41 +0100

    refactor: use wrapper structure

--- Package.swift
+++ Package.swift
@@ -4,32 +4,34 @@
 import PackageDescription
 
 let package = Package(
-    name: "MobaiBiometric",
+    name: "MobaiSPM",
     products: [
         .library(
             name: "MobaiBiometric",
-            targets: ["MobaiBiometric", "MobaiBiometricCore"]
+            targets: ["MobaiBiometricWrapper"]
         ),
     ],
     dependencies: [
         .package(
+            name: "SwiftProtobuf",
             url: "https://github.com/apple/swift-protobuf.git",
             "1.19.0" ..< "2.0.0"
         ),
     ],
     targets: [
+        .target(
+            name: "MobaiBiometricWrapper",
+            dependencies: [
+                .target(name: "MobaiBiometric"),
+                "SwiftProtobuf"
+            ],
+            path: "Sources/MobaiBiometricCore"
+        ),
         .binaryTarget(
             name: "MobaiBiometric",
             url: "https://downloads.mobai.dev/releases/ios/2.1.2-beta.2/MobaiBiometric.xcframework.zip",
             checksum: "44e1b5fc954415c6f0901fc61411ef1ad13c7999d43a8d2a93a0d62260dcdfab"
         ),
-        .target(
-            name: "MobaiBiometricCore",
-            dependencies: [
-                "MobaiBiometric",
-                .product(name: "SwiftProtobuf", package: "swift-protobuf")
-            ],
-            path: "Sources/MobaiBiometricCore"
-        ),
+        
     ]
 )
