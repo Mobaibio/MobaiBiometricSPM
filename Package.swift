c70685b9f432219e2d2e4de1ee775e1dfa67e93b
Martin <martin@mobai.bio>
2024-11-26 22:22:21 +0100

    refactor: use dynamic linking

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "MobaiBiometric",
+            type: .dynamic,
             targets: ["MobaiBiometricWrapper"]
         ),
     ],
