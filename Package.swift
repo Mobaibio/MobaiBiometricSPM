8b0ae66ab58730ef71ed6bfaf6023456f76c3cc3
Dion Dula <diondula@Dions-MacBook-Pro.local>
2026-01-13 13:32:03 +0100

    feat: add nfc and documents spm package

--- Package.swift
+++ Package.swift
@@ -5,17 +5,35 @@ import PackageDescription
 
 let package = Package(
     name: "MobaiBiometric",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "MobaiBiometric",
             targets: ["MobaiBiometric"]),
+        .library(
+            name: "MobaiNFC",
+            targets: ["MobaiNFC"]),
+        .library(
+            name: "MobaiDocument",
+            targets: ["MobaiDocument"]),
     ],
     targets: [
         .binaryTarget(
             name: "MobaiBiometric",
             url: "https://downloads.mobai.dev/releases/ios/2.3.1/MobaiBiometric.xcframework.zip",
             checksum: "ca30db0b12bcf807b858ed65ed3366af9ef0fdd681a57b748f9c30d761f4116c"
-
+        ),
+        .binaryTarget(
+            name: "MobaiNFC",
+            url: "https://downloads.mobai.dev/releases/ios/1.1.0/MobaiNfc.xcframework.zip",
+            checksum: "2a917eac0117fa88b060e702ffefd5d3ea26b5c4ef3a5e47c3db19caa32254fa"
+        ),
+        .binaryTarget(
+            name: "MobaiDocument",
+            url: "https://downloads.mobai.dev/releases/ios/1.1.0/MobaiDocument.xcframework.zip",
+            checksum: "73bf6dfae944c1c797151eba91b51b63c1f906e2303fc38d25849182512ba799"
         ),
     ]
 )
